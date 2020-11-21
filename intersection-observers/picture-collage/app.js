be88f7038965ea71e9fa2e59dbeeedfd5f7a46ad
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2020-11-21 10:01:24 -0600

    loading in pictures on grid. Starting point for intersection observers.

--- intersection-observers/picture-collage/app.js
+++ intersection-observers/picture-collage/app.js
@@ -1,10 +1,21 @@
+const unsplashUrl = 'https://source.unsplash.com/random/';
 
-getNextImages(15);
+const rootDiv = document.querySelector('.grid');
 
+getNextImages(60).forEach(image => {
+  let imageDiv = createImageDiv(image);
+  let img = createImg(image);
 
+  console.log('span columns ' + getSpanLength(image.height));
+  console.log('span rows ' + getSpanLength(image.width));
 
+  imageDiv.appendChild(img);
+  rootDiv.appendChild(imageDiv);
+});
+
+// gets dimentions random dimensions for image
 function getNextImages(n){
-  Array.from({length: n}, () => {
+  return Array.from({length: n}, () => {
     const image = {
       width: randomSize(),
       height: randomSize()
@@ -15,13 +26,32 @@ function getNextImages(n){
   });
 }
 
-function getImageDimensions() {
+function createImageDiv(image){
+  let imageDiv = document.createElement('div');
+  imageDiv.style.cssText = `
+    --image-height: ${getSpanLength(image.height)}; 
+    --image-width: ${getSpanLength(image.width)};
+  `;
+  imageDiv.classList.add('grid-item');
 
+  return imageDiv;
+}
+
+function createImg(image) {
+  let img = document.createElement('img');
+  img.src = `${unsplashUrl}${image.width}x${image.height}`;
+
+  return img;
+}
+
+// this will be how many coluns or rows the image spans on the grid
+function getSpanLength(n){
+  return Math.floor((n % 4) + 1)
 }
 
 function randomSize() {
-  const max = 400;
-  const min = 200;
+  const max = 1600;
+  const min = 800;
 
   let n = Math.random() * (max - min) + min;  // generate random num between
   return Math.round(n / 10) * 10; // round numb to nearest 10
