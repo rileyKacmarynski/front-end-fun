f9f9698804936f4dc2770a4dbf4a082f2afbae53
Riley Kacmarynski <rkacmarynski@omnitech-inc.com>
2020-11-25 16:24:18 -0600

    finish desktop layout.

--- room-homepage/app.js
+++ room-homepage/app.js
@@ -3,6 +3,4 @@ const nav = document.querySelector('#nav-toggle');
 nav.addEventListener('click', () => {
   body.classList.toggle('nav-open');
   const navLinks = document.querySelector('.nav-links');
-  const isShowing = navLinks.getAttribute('aria-hidden') == 'false';
-  navLinks.setAttribute('aria-hidden', isShowing);
 });
